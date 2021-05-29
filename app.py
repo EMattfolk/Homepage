5704920586974a6e44e2da5f091d1faf50fc738e
Erik Mattfolk <erik.mattfolk@gmail.com>
2021-05-29 17:27:45 +0200

    Style the website

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path="/static")
 
+def page(name: str):
+    with open(f"pages/{name}.html") as f:
+        return render_template("base.html", content=f.read())
+
 @app.route("/")
 def root():
-    return render_template("index.html")
+    return page("about")
