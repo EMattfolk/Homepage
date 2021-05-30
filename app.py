6759dfa2d78e448fb0177ba6ce72abe28bdbde60
Erik Mattfolk <erik.mattfolk@gmail.com>
2021-05-30 12:10:31 +0200

    Add navigation

--- app.py
+++ app.py
@@ -2,10 +2,35 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path="/static")
 
+navigation = [
+    ("Home",     "/"),
+    ("CV",       "/cv"),
+    ("Projects", "/projects"),
+];
+
 def page(name: str):
     with open(f"pages/{name}.html") as f:
-        return render_template("base.html", content=f.read())
+        content = f.read()
+
+    kwargs = {
+        "content": content,
+        "navigation": navigation,
+        "page": name,
+    }
+
+    return render_template("base.html", **kwargs)
+
 
 @app.route("/")
-def root():
-    return page("about")
+def home():
+    return page("home")
+
+
+@app.route("/cv")
+def cv():
+    return page("cv")
+
+
+@app.route("/projects")
+def projects():
+    return page("projects")
